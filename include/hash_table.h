634fcfb1053d7ad43a5f80342c575cf9db19cec3
tenet <1697437561@qq.com>
2022-03-22 13:31:52 +0800

    add hash_table

--- include/hash_table.h
+++ include/hash_table.h
@@ -1,34 +1,49 @@
 #pragma once
 
+#include <stdint.h>
+
 #define CACHE_LINE_SIZE 64
 #define ENTRY_NUMBER 6
 #define HASH_TABLE_SIZE 101
 
 struct hash_bucket_node;
-struct hash_bucket;
+struct hash_bucket
+{
+    struct hash_bucket_node *head;
+};
 
-struct hash_table {
+typedef uint32_t bucket_index_t;
+
+struct hash_table
+{
 	struct hash_bucket bucket_list[HASH_TABLE_SIZE];
 
-	/* 
-	* 保护bucket的锁
-	* 可以一个bucket一个锁，也可以相邻多个bucket一个锁
-	*/
+	/*
+	 * 保护bucket的锁
+	 * 可以一个bucket一个锁，也可以相邻多个bucket一个锁
+	 */
 
-	int (*compare_key)(void *key1, void *key2);
+	int (*compare_hash_key)(void *key1, void *key2);
+	void *(*get_hash_key)(void *value);
+	bucket_index_t (*hash)(void *key);
 };
+
+enum hash_operation_exceptions
+{
+	alloc_bucket_node_fail = 1
+};
+
+void hash_table_init(struct hash_table *self, int (*cmp)(void *, void *), void *(*getkey)(void *), bucket_index_t (*hash_fun)(void *));
 
 // 获取关键字key在哈希表中的桶
 struct hash_bucket *hash_table_get_bucket(struct hash_table *self, void *key);
 
-// 在桶中查找关键字为key的值
-void *hash_bucket_lookup_value(struct hash_bucket *self, void *key, int (*comparator)(void *, void *));
+// 在给定桶中查找并返回关键字为key的value，不存在返回NULL
+// 若pvalue非空，则将其置为value的地址
+void *hash_bucket_lookup_value(struct hash_table *self, struct hash_bucket *hb, void *key, unsigned long *pvalue);
 
-// 在桶中删除关键字为key的元素
-int hash_bucket_delete_value(struct hash_bucket *self, void *key, int (*comparator)(void *, void *));
+// 将value插入桶中（不判断value的key是否对应这个桶）
+int hash_bucket_insert_value(struct hash_bucket *self, void *value);
 
-// 将value插入桶hb中（不判断value的key是否对应这个桶）
-void hash_bucket_insert_value(struct hash_bucket *self, void *value);
-
-// 删除桶结点中下标为entry的项
-void hash_bucket_node_delete_entry(struct hash_bucket_node *hbn, int entry);
+// 删除桶结点中pentry指向的项
+void hash_bucket_node_delete_entry(unsigned long pentry);
--- src/hash_table.c
+++ src/hash_table.c
@@ -1,15 +1,152 @@
 #include "hash_table.h"
-#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
-struct hash_bucket {
-	struct hash_bucket_node *head;
-};
+typedef uint8_t bitset_t;
 
-typedef uint32_t bitset_t;
-#pragma pack(CACHE_LINE_SIZE)
-struct hash_bucket_node {
-	void *value[ENTRY_NUMBER];
-	struct HashBucketNode *next;
-	bitset_t bitset;
-};
-#pragma pack()
+struct hash_bucket_node
+{
+    void *value[ENTRY_NUMBER];
+    struct hash_bucket_node *next;
+    bitset_t bitset; // 1:可用 0:已占用
+} __attribute__((aligned(CACHE_LINE_SIZE)));
+
+#define set_bit(s, n) (s) |= ((bitset_t)1 << (n))
+#define reset_bit(s, n) (s) &= (~((bitset_t)1 << (n)))
+#define is_set_bit(s, n) ((s) & ((bitset_t)1 << (n)))
+#define is_reset_bit(s, n) (!((s) & ((bitset_t)1 << (n))))
+
+struct hash_bucket_node* alloc_bucket_node()
+{
+    // 之后可能不用malloc，自己管理缓存池
+    return malloc(sizeof(struct hash_bucket_node));
+}
+
+void bucket_node_init(struct hash_bucket_node *self)
+{
+    self->bitset = 0;
+    self->next = NULL;
+    for (int i = 0; i < ENTRY_NUMBER; ++i)
+    {
+        self->value[i] = NULL;
+        set_bit(self->bitset, i);
+    }
+}
+
+int bucket_node_get_next_used_entry(struct hash_bucket_node *self, int *entry)
+{
+    int index = *entry + 1;
+    for (; index < ENTRY_NUMBER; ++index)
+        if (is_reset_bit(self->bitset, index))
+            break;
+    *entry = index;
+    return index == ENTRY_NUMBER ? -1 : 0;
+}
+
+int bucket_node_get_unused_entry(struct hash_bucket_node *self)
+{
+    int entry = 0;
+    for (; entry < ENTRY_NUMBER; ++entry)
+        if (is_set_bit(self->bitset, entry))
+            break;
+    return entry == ENTRY_NUMBER ? -1 : entry;
+}
+
+static inline void bucket_node_set_entry(struct hash_bucket_node *self, int entry, void *value)
+{
+    reset_bit(self->bitset, entry);
+    self->value[entry] = value;
+}
+
+// 给定指针value，返回它所属bucket_node的地址
+static inline unsigned long value_ptr_to_bucket_node_ptr(unsigned long value)
+{
+    unsigned long mask = ~((unsigned long)CACHE_LINE_SIZE - 1);
+    return value & mask;
+}
+
+// 给定指向node中value数组元素的指针value，返回该元素下标
+static inline int value_ptr_to_bucket_node_value_index(unsigned long value)
+{
+    return (value - value_ptr_to_bucket_node_ptr(value)) / sizeof(void *);
+}
+
+void hash_bucket_node_delete_entry(unsigned long pentry)
+{
+    int index = value_ptr_to_bucket_node_value_index(pentry);
+    struct hash_bucket_node *node = (struct hash_bucket_node*)value_ptr_to_bucket_node_ptr(pentry);
+    set_bit(node->bitset, index);
+    node->value[index] = NULL;
+}
+
+static inline void hash_bucket_insert_node(struct hash_bucket *self, struct hash_bucket_node *node)
+{
+    node->next = self->head;
+    self->head = node;
+}
+
+void hash_table_init(struct hash_table *self, int (*cmp)(void *, void *), void *(*getkey)(void *), bucket_index_t (*hash_fun)(void *))
+{
+    memset(self->bucket_list, 0, sizeof(self->bucket_list));
+    self->compare_hash_key = cmp;
+    self->get_hash_key = getkey;
+    self->hash = hash_fun;
+}
+
+struct hash_bucket *hash_table_get_bucket(struct hash_table *self, void *key)
+{
+    bucket_index_t index = self->hash(key) % HASH_TABLE_SIZE;
+    return &(self->bucket_list[index]);
+}
+
+void *hash_bucket_lookup_value(struct hash_table *self, struct hash_bucket *hb, void *key, unsigned long *pvalue)
+{
+    void *result = NULL;
+    struct hash_bucket_node *node = hb->head;
+    int entry = -1;
+    while (node != NULL) // 遍历冲突链中所有的BucketNode
+    {
+        // 找到当前node中下一个被占用的项
+        int res = bucket_node_get_next_used_entry(node, &entry);
+        if (res == -1) // 当前BucketNode中已经找完了，在下一个node中继续找
+        {
+            node = node->next;
+            entry = -1;
+            continue;
+        }
+        void *entry_key = self->get_hash_key(node->value[entry]);
+        if (self->compare_hash_key(entry_key, key) == 0) // 比较key, 返回0表示相等，即找到目标
+        {
+            result = node->value[entry];
+            if (pvalue != NULL)
+                *pvalue = (unsigned long)&(node->value[entry]);
+            break;
+        }
+    }
+    return result;
+}
+
+int hash_bucket_insert_value(struct hash_bucket *self, void *value)
+{
+    struct hash_bucket_node *node = self->head;
+    for (;;)
+    {
+        if (node == NULL)
+        {
+            node = alloc_bucket_node();
+            if (node == NULL)
+                return alloc_bucket_node_fail;
+            bucket_node_init(node);
+            hash_bucket_insert_node(self, node);
+        }
+        int index = bucket_node_get_unused_entry(node);
+        if (index == -1)
+        {
+            node = node->next;
+            continue;
+        }
+        bucket_node_set_entry(node, index, value);
+        break;
+    }
+    return 0;
+}
